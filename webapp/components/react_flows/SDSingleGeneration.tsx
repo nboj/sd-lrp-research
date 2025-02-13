e80ed3fe0740595a0c148e00f1bdca3d30c50996
nboj <christian.auman@outlook.com>
2025-02-13 10:41:34 -0500

    fixing build errors

--- webapp/components/react_flows/SDSingleGeneration.tsx
+++ webapp/components/react_flows/SDSingleGeneration.tsx
@@ -20,7 +20,7 @@ const PopupBody = ({ node_id }: any) => {
                 <>
                     <ModalHeader className="flex flex-col gap-1">Predicted Noise</ModalHeader>
                     <ModalBody>
-                        {/*@ts-ignore*/}
+                        {/*@ts-expect-error*/}
                         <SingleGenerationRemake />
                     </ModalBody>
                 </>
